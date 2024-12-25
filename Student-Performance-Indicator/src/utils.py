056e06de6a8d9ac11a0fa5d424b18f82d0410ea1
saadtariq-ds <tariqsaad1997@gmail.com>
2024-12-26 02:48:15 +0500

    Add prediction pipeline and flask app

--- Student-Performance-Indicator/src/utils.py
+++ Student-Performance-Indicator/src/utils.py
@@ -103,3 +103,11 @@ def model_hyperparameters():
             'iterations': [30, 50, 100]
         },
     }
+
+def load_object(file_path):
+    try:
+        with open(file=file_path, mode='rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
